9f9a345925174229c0a8f11e96da71650a409ff7
emmanuel Mensah <emmaxp183@gmail.com>
2022-02-14 12:49:38 +0100

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by admin on 14/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
